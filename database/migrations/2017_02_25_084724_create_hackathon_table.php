3dac4c0e91e8b045ac8a8c1b3cf435a9088dd9e4
Kyle Eisenbarger <keisenb@ksu.edu>
2017-02-25 03:19:38 -0600

    Setup Database

---
+++ database/migrations/2017_02_25_084724_create_hackathon_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHackathonTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+      Schema::create('hackathon', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('name');
+      $table->date('start_date');
+      $table->date('end_date');
+      $table->string('address');
+      $table->string('facebook');
+      $table->string('twitter');
+      $table->string('instagram');
+      $table->string('state');
+      $table->unsignedInteger('zip');
+      $table->string('city');
+      $table->dateTime('registration_begin');
+      $table->dateTime('registration_end');
+      $table->dateTime('checkin_begin');
+      $table->dateTime('checkin_end');
+      $table->integer('current_balance');
+      $table->integer('created_by')->unsigned();
+      $table->integer('updated_by')->unsigned();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::disableForeignKeyConstraints();
+    Schema::drop('hackathon');
+  }
+}
