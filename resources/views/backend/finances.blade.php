d643a5f1e506efffc67d8120023397c62dc90d87
Ashley Coleman <ashley78@ksu.edu>
2017-02-25 04:23:43 -0600

    progress on page ui

--- resources/views/backend/finances.blade.php
+++ resources/views/backend/finances.blade.php
@@ -10,6 +10,12 @@
     </div>
     @include('subviews/messages')
   </div>
+
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h1 class="text-danger">-$500</h1>
+    </div>
+  </div>
   <div class="hr-divider m-t-md m-b">
     <h3 class="hr-divider-content hr-divider-heading">Finances Brother</h3>
   </div>
@@ -19,21 +25,25 @@
       <thead>
         <tr>
           <th>Name</th>
-          <th>Amount</th>
+          <th class="text-right">Amount</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>Bats</td>
-          <td>1000</td>
+          <td class="text-right">1000</td>
+          <td><span class="icon icon-edit"></span></td>
         </tr>
         <tr>
           <td>Thunderhead</td>
-          <td>200</td>
+          <td class="text-right">200</td>
+          <td><span class="icon icon-edit"></span></td>
         </tr>
         <tr>
           <td>Mobile Development Club</td>
-          <td>100</td>
+          <td class="text-right">100</td>
+          <td><span class="icon icon-edit"></span></td>
         </tr>
       </tbody>
       </table>
@@ -43,24 +53,34 @@
       <thead>
         <tr>
           <th>Name</th>
-          <th>Amount</th>
+          <th class="text-right">Amount</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>Amazon Dots</td>
-          <td>-500</td>
+          <td class="text-right">-500</td>
+          <td><span class="icon icon-edit"></span></td>
         </tr>
         <tr>
           <td>Jimmy Johns</td>
-          <td>-300</td>
+          <td class="text-right">-300</td>
+          <td><span class="icon icon-edit"></span></td>
         </tr>
         <tr>
           <td>Goodcents</td>
-          <td>-280</td>
+          <td class="text-right">-280</td>
+          <td><span class="icon icon-edit"></span></td>
         </tr>
       </tbody>
       </table>
     </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-3"></div>
+    <button type="button" class="btn btn-lg btn-default col-md-6">New Entree</button>
+    <div class="col-md-3"></div>
   </div>
 @endsection
