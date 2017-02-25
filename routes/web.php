d685278c01945c3969ef1d8e308f3bc1a4956185
Kyle Eisenbarger <keisenb@ksu.edu>
2017-02-25 03:47:47 -0600

    Added models

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@
 
 Route::get('/', function () {return view('welcome');});
 Route::get('/home', 'HomeController@index');
-Route::get('/dashboard', function () {return view('backend/dashboard');});
+Route::get('/dashboard', 'DashboardController@Dashboard');
 
 Auth::routes();
