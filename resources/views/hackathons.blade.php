566dfb7990d7e505e04fc9f50be9c1f76f2797f6
Kyle Eisenbarger <keisenb@ksu.edu>
2017-02-25 22:39:06 -0600

    Added link to hackathons

--- resources/views/hackathons.blade.php
+++ resources/views/hackathons.blade.php
@@ -6,15 +6,17 @@
     <h1>Hackathons</h1>
     <div class="row">
     @foreach($hackathons as $h)
-    <div class="col-md-6 col-lg-6 m-b">
-      <div class="statcard {{ $color_strings[array_rand($color_strings, 1)] }}">
-        <div class="p-a">
-          <span class="statcard-desc">{{ $h->city . ", " . $h->state }}</span>
-          <h2 class="statcard-number">{{ $h->name }}</h2>
-          <small class="delta-indicator">{{ date("M d, Y", strtotime($h->start_date)) }} - {{ date("M d, Y", strtotime($h->end_date)) }}</small>
+    <a href="{{ url('/dashboard/events/' . $h->id )}}">
+      <div class="col-md-6 col-lg-6 m-b">
+        <div class="statcard {{ $color_strings[array_rand($color_strings, 1)] }}">
+          <div class="p-a">
+            <span class="statcard-desc">{{ $h->city . ", " . $h->state }}</span>
+            <h2 class="statcard-number">{{ $h->name }}</h2>
+            <small class="delta-indicator">{{ date("M d, Y", strtotime($h->start_date)) }} - {{ date("M d, Y", strtotime($h->end_date)) }}</small>
+          </div>
         </div>
       </div>
-    </div>
+    </a>
     @endforeach
   </div>
   </div>
