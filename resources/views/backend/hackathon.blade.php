cddb36b5bda6c69206c4251ceacc55aa1f542f5f
Kyle Eisenbarger <keisenb@ksu.edu>
2017-02-25 23:27:15 -0600

    Added features to hackathon page

--- resources/views/backend/hackathon.blade.php
+++ resources/views/backend/hackathon.blade.php
@@ -36,6 +36,34 @@
   <h3 class="hr-divider-content hr-divider-heading">Details</h3>
 </div>
 
+<div class="container">
+<div class="row">
+  <div class="col-sm-5 col-sm-offset-2 col-md-6 col-md-offset-3">
+    <button type="button" class="btn btn-primary btn-lg col-md-5" display="block" >Sign Up</button>
+</div>
+  </div>
+  <div class="row">
+    <div class="col-sm-5 col-sm-offset-2 col-md-3 col-md-offset-0">
+      <h3><span class="label label-info">
+        <small class="delta-indicator">{{ date("M d, Y", strtotime($hackathon->registration_begin)) }}</small>
+      </span>Registration Opens</h3>
+    </div>
+
+     <div class="col-sm-5 col-md-4 col-md-offset-0">
+       <h3><span class="label label-success">
+         <small class="delta-indicator">{{ date("M d, Y", strtotime($hackathon->start_date)) }}</small>
+</span>Start Time</h3>
+     </div>
+     <div class="col-sm-5 col-sm-offset-2 col-md-2 col-md-offset-0">
+       <h3><span class="label label-danger">
+         <small class="delta-indicator">{{ date("M d, Y", strtotime($hackathon->end_date)) }}</small>
+       </span>End Time</h3>
+     </div>
+
+
+  </div>
+
+</div>
 
 
 
